6252082015ebe49bfa7dbe304104e82ce9672845
Isabela Acosta <118640598+isabelaacr@users.noreply.github.com>
2025-08-13 16:03:33 -0300

    Update extra.vhdl

--- 13-08/extra.vhdl
+++ 13-08/extra.vhdl
@@ -1,1 +1,24 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
 
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity ISA is
+    Port ( a, b, c: in  STD_LOGIC;
+           x : inout STD_LOGIC; 
+			  y : out  STD_LOGIC);
+end ISA;
+
+architecture Behavioral of ISA is
+
+begin
+x <= ((a and b) or c);
+y <= (x xor c);
+end Behavioral;
